4047bf3bfcd46a75319fcec19e03528b6f7acd5d
jack <biffisbuffm@gmail.com>
2020-07-23 17:51:04 -0500

    refactor

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-// Notice the lack of parentheses around the 'profileDataAr' part
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-console.log('=====================');
+const profileDataArgs = process.argv.slice(2);
 
-profileDataArr.forEach(profileItem =>
-    console.log(profileItem));
+const [name, github] = profileDataArgs;
 
 
-const printProfileData = (profileDataArr) => {
-    for (let i=0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    };
-};
 
-printProfileData(profileDataArgs);
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
