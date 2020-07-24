1c0f4b65fcc56205cdbc500f8581a5635d1b43fc
jack <biffisbuffm@gmail.com>
2020-07-24 14:26:07 -0500

    typo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // const fs = require('fs');
-const generatePage = require('./src/page-template.jsoed');
+const generatePage = require('./src/page-template.js');
 
 // const profileDataArgs = process.argv.slice(2);
 
