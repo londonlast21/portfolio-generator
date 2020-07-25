d7583493998e7f881e21be6c4fe93f2c7826893b
jack <biffisbuffm@gmail.com>
2020-07-25 18:09:22 -0500

    aaa

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const generateProjects = projectsArr => {
     </section>
   `;
 };
-const generatePage=  require('./src/page-template.js');
+const generatePage =  require('./src/page-template.js');
 
 // const profileDataArgs = process.argv.slice(2);
 
@@ -170,9 +170,20 @@ promptUser()
   .then(portfolioData => {
     const pageHTML = generatePage(portfolioData);
 
-     fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
+     fs.writeFile('./dist/index.html', pageHTML, err => {
+      if (err) {
+        console.log(err);
+        return;
+      }
 
        console.log('Page created! Check out index.html in this directory to see it!');
+
+       fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err) {
+          console.log(err);
+          return;
+        } 
+        console.log('Style sheet copied successfully!');
+       });
      });
   });
